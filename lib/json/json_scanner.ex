bea3472ce2d26764eaf3ca1e57480d536ce1b187
Dickson S. Guedes <guedes@guedesoft.net>
2012-04-25 23:25:22 -0300

    initial porting rfc4627 from Erlang to Elixir

--- lib/json/json_scanner.ex
+++ lib/json/json_scanner.ex
@@ -1,4 +1,37 @@
 defmodule JSON.Scanner do
+  @moduledoc """
+  This module is an Elixir porting of the
+  JSON RFC 4627 implementation for Erlang.
+
+  See: https://github.com/tonyg/erlang-rfc4627/blob/master/src/rfc4627.erl
+
+  Erlang version's authors:
+  - Tony Garnock-Jones <tonygarnockjones@gmail.com>
+  - LShift Ltd. <query@lshift.net>
+
+  Elixir version's author:
+    Dickson S. Guedes <guedes@guedesoft.net>
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation
+  files (the "Software"), to deal in the Software without
+  restriction, including without limitation the rights to use, copy,
+  modify, merge, publish, distribute, sublicense, and/or sell copies
+  of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  """
 
   def decode(binary) when is_binary(binary) do
     decode(binary_to_list(binary))
@@ -6,7 +39,8 @@ defmodule JSON.Scanner do
 
   def decode(chars) when is_list(chars) do
     { value, remaining } = parse(ignore_spaces(chars))
-    { :ok, value, ignore_spaces(remaining) }
+    {_ ,  json_object } = value
+    json_object
   end
 
   def parse([?" | rest ]) do
@@ -19,7 +53,7 @@ defmodule JSON.Scanner do
   def parse("null" <> rest),  do: { nil,    rest }
   def parse([?{ | rest]), do: parse_object(ignore_spaces(rest), [])
   def parse([]), do: "xxxxx"
-  def parse(chars), do: "yyyy"
+  def parse(chars), do: parse_number(chars, [])
 
   def parse_string(chars, acc) do
     case parse_codepoint(chars) do
@@ -36,6 +70,27 @@ defmodule JSON.Scanner do
 
   def parse_general_char(?", rest), do: { :ok, ?", rest }
 
+  def parse_number(a, b) do
+    999
+  end
+
+  #def parse_number([?- | rest], acc) do
+  #  parse_signed_number(rest, [?- | acc])
+  #end
+
+  #def parse_signed_number(rest, acc) do
+  #  { acc, rest } = parse_int_part(rest, acc)
+  #  case rest do
+  #    match: []
+  #      finish_number( acc, [])
+  #    match: [?. | more]
+  #      { acc, rest } = parse_int_part(more, [?. | acc])
+  #      parse_exp(rest, acc, false)
+  #    else:
+  #      parse_exp(acc, rest)
+  #  end
+  #end
+
   def parse_object([?} | rest ], acc) do
     { {:obj, List.reverse(acc) }, acc }
   end
@@ -51,48 +106,48 @@ defmodule JSON.Scanner do
     parse_object(ignore_spaces(rest), [ { key_codepoints, value } | acc ])
   end
 
-  
-  def json_object("{}"), do: []
 
-  def json_object(object) do
-    regex = %r/\{(.+)\}/
+  #def json_object("{}"), do: []
 
-    destructure([_, members ], Regex.run(regex, object))
-    [ json_pair(members) ]
-  end
+  #def json_object(object) do
+  #  regex = %r/\{(.+)\}/
 
-  def json_pair(pair) do
-    regex = %r/^"(.+)":(.+)$/
-    destructure([ _, key, value ], Regex.run(regex, pair))
-    { json_string(key, :as_key), json_value(value) }
-  end
+  #  destructure([_, members ], Regex.run(regex, object))
+  #  [ json_pair(members) ]
+  #end
 
-  def json_string(string, :as_key), do: binary_to_atom(string)
-  def json_string(string), do: string
+  #def json_pair(pair) do
+  #  regex = %r/^"(.+)":(.+)$/
+  #  destructure([ _, key, value ], Regex.run(regex, pair))
+  #  { json_string(key, :as_key), json_value(value) }
+  #end
 
-  def json_value("[]"), do: []
+  #def json_string(string, :as_key), do: binary_to_atom(string)
+  #def json_string(string), do: string
 
-  def json_value("\"" <> value) do
-    regex = %r/^(.+)"$/
-    destructure([ _, string ], Regex.run(regex, value))
-    string
-  end
+  #def json_value("[]"), do: []
 
-  def json_value("[" <> value) do
-    regex = %r/^(.+)]$/
-    destructure([ _, array ], Regex.run(regex, value))
-    json_array(array)
-  end
+  #def json_value("\"" <> value) do
+  #  regex = %r/^(.+)"$/
+  #  destructure([ _, string ], Regex.run(regex, value))
+  #  string
+  #end
 
-  def json_array(array) do
-    result = Regex.split %r/,/, array
+  #def json_value("[" <> value) do
+  #  regex = %r/^(.+)]$/
+  #  destructure([ _, array ], Regex.run(regex, value))
+  #  json_array(array)
+  #end
 
-    if Regex.match?(%r/^(\d,?)+$/, array) do
-      Enum.map(result, fn(x) -> list_to_integer(binary_to_list(x)) end)
-    end
-  end
+  #def json_array(array) do
+  #  result = Regex.split %r/,/, array
+
+  #  if Regex.match?(%r/^(\d,?)+$/, array) do
+  #    Enum.map(result, fn(x) -> list_to_integer(binary_to_list(x)) end)
+  #  end
+  #end
 
   defp ignore_spaces(string) when is_binary(string), do: ignore_spaces(binary_to_list(string))
   defp ignore_spaces([ c | rest ]) when c <= 32, do: ignore_spaces(rest)
-  defp ignore_spaces(list), do: list_to_binary(list)
+  defp ignore_spaces(chars), do: chars
 end
--- test/json_scanner_test.exs
+++ test/json_scanner_test.exs
@@ -1,29 +1,29 @@
 Code.require_file "../test_helper", __FILE__
 
-defmodule JSON.Scanner.ScanTest do
-  use ExUnit.Case
-
-  test :empty_json do
-    assert_equal [], JSON.Scanner.json_object("{}")
-  end
-
-  test :simple_kv do
-    assert_equal [ k: "v" ], JSON.Scanner.json_object("{\"k\":\"v\"}")
-  end
-
-  test :string_pair do
-    assert_equal { :k, "v" }, JSON.Scanner.json_pair("\"k\":\"v\"")
-  end
-
-  test :empty_array_pair do
-    assert_equal { :k, [] }, JSON.Scanner.json_pair("\"k\":[]")
-  end
-
-  test :number_array_pair do
-    assert_equal { :k, [ 1, 2, 30 ] }, JSON.Scanner.json_pair("\"k\":[1,2,30]")
-  end
-
-end
+#defmodule JSON.Scanner.ScanTest do
+#  use ExUnit.Case
+#
+#  test :empty_json do
+#    assert_equal [], JSON.Scanner.json_object("{}")
+#  end
+#
+#  test :simple_kv do
+#    assert_equal [ k: "v" ], JSON.Scanner.json_object("{\"k\":\"v\"}")
+#  end
+#
+#  test :string_pair do
+#    assert_equal { :k, "v" }, JSON.Scanner.json_pair("\"k\":\"v\"")
+#  end
+#
+#  test :empty_array_pair do
+#    assert_equal { :k, [] }, JSON.Scanner.json_pair("\"k\":[]")
+#  end
+#
+#  test :number_array_pair do
+#    assert_equal { :k, [ 1, 2, 30 ] }, JSON.Scanner.json_pair("\"k\":[1,2,30]")
+#  end
+#
+#end
 
 defmodule JSON.Scanner.ParseTest do
   use ExUnit.Case
@@ -31,29 +31,24 @@ defmodule JSON.Scanner.ParseTest do
   test :empty_json do
     assert_equal [], JSON.Scanner.decode("{}")
   end
-  
+
   test :simple_kv do
-    assert_equal { :ok, [ k: "v" ], [] }, JSON.Scanner.decode('{"k":"v"}')
+    assert_equal [ {'key', "value" } ], JSON.Scanner.decode('{"key":"value"}')
   end
 
-  #  test :simple_kv do
-  #    assert_equal [ k: "v" ], JSON.Scanner.parse("{\"k\":\"v\"}")
-  #  end
-
-  #test :ignore_space do
-  #  assert_equal "joao", JSON.Scanner.ignore_spaces("   joao")
-  #end
-  #
-  #  test :string_pair do
-  #    assert_equal { :k, "v" }, JSON.Scanner.json_pair("\"k\":\"v\"")
-  #  end
-  #
-  #  test :empty_array_pair do
-  #    assert_equal { :k, [] }, JSON.Scanner.json_pair("\"k\":[]")
-  #  end
-  #
-  #  test :number_array_pair do
-  #    assert_equal { :k, [ 1, 2, 30 ] }, JSON.Scanner.json_pair("\"k\":[1,2,30]")
-  #  end
-
+  test :rfc4267_json_object do
+    # See http://www.ietf.org/rfc/rfc4627.txt
+    assert_equal       [ image: [
+          width: 800,
+          height: 600,
+          title: "View from 15th Floor",
+          thumbnail: [
+            url: "http://www.example.com/image/481989943",
+            height: 125,
+            width: 100
+          ],
+          ids: [ 116, 943, 234, 38793 ]
+        ]
+      ], JSON.Scanner.decode('{"image":{"height":600,"ids":[116,943,234,38793],"thumbnail":{"height":125,"url":"http://www.example.com/image/481989943","width":100},"title":"View from 15th Floor","width":800}}')
+  end
 end
