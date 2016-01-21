689b860cc404bd1a9c183e626d952399a5706106
Dickson S. Guedes <guedes@guedesoft.net>
2016-01-21 13:33:30 -0200

    fix deprecations, closes #28, #33

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule ExJSON.Mixfile do
 
   def project do
     [ app: :exjson,
-      version: "0.3.0",
-      elixir: "~> 0.13.0",
+      version: "0.4.0",
+      elixir: "~> 1.1.0",
       description: description,
       package: package,
       deps: deps ]
